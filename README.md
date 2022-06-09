656cb29b0049ae97ac7e62393644d828ea3cf3ba
Radik Valeev <radik-v@mail.ru>
2022-06-10 00:04:32 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# env-test
+
+yarn set version stable
+
+See [ref](https://cli.vuejs.org/ru/guide/mode-and-env.html#переменные-окружения).
---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <H2>{{ title }}</H2>
+    <p>URL:{{ url }}</p>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  data() {
+    console.log(process.env);
+    return {
+      url: process.env.VUE_APP_URL,
+      title: process.env.VUE_APP_TITLE,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
